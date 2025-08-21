8dbc5003977807dfea6ecf48f1b2b4ecb3bcd9d7
Luka Khangoshvili <lukakhangoshvili@gmail.com>
2025-08-21 18:10:39 +0400

    parsing incoming requests

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.use('/add-product', (req, res, next) => {
-    console.log('In the middleware');
-    res.send('<form action="/" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>');
 });
 
 app.use('/product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
-})
+});
 
 app.use('/', (req, res, next) => {
-    console.log('In the middleware');
-    res.send('<h1>Home page</h1>');
+    res.send('<h1>Home from Express!</h1>');
 });
 
 app.listen(3000);
