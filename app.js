0923d268df206bf59885745adad6d30a68605a4f
Luka Khangoshvili <lukakhangoshvili@gmail.com>
2025-08-21 17:43:19 +0400

    add product routes

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ const express = require('express');
 
 const app = express();
 
-app.use('/', () => {
-
-})
-
 app.use('/add-product', (req, res, next) => {
     console.log('In the middleware');
-    res.send('<form></form>');
+    res.send('<form action="/" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>');
 });
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
 
 app.use('/', (req, res, next) => {
     console.log('In the middleware');
